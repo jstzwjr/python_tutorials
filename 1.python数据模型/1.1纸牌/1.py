55a76cb94e83c1fe20fbc6d2a64e76444e27d038
jstzwjr <wangjianrongtz@163.com>
2021-08-14 14:44:17 +0800

    add 1.py

---
+++ 1.python数据模型/1.1纸牌/1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/14 14:28
+# @Author  : wangjianrong
+# @File    : 1.py
+
+import collections
+from itertools import product
+
+Card = collections.namedtuple("Card", ["rank", "suit"])
+
+
+class FrenchRank:
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    suits = 'spades diamonds clubs hearts'.split()  # 花色
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for rank, suit in product(self.ranks, self.suits)]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, item):
+        return self._cards[item]
+
+
+if __name__ == '__main__':
+    card = Card('8', 'diamonds')
+    print(card.rank)
+    print(card.suit)
+
